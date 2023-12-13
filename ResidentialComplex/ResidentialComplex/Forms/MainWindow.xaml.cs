f9afdbd314a108dcd109c5ac1dfaf8e85e83d292
Taisia Noskova <cloutduck@taigo.xyz>
2023-12-13 12:58:33 +0600

    Создали новые страницы.  Сделали обработчики событий по нажатиям на кнопки.

--- ResidentialComplex/ResidentialComplex/Forms/MainWindow.xaml.cs
+++ ResidentialComplex/ResidentialComplex/Forms/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ResidentialComplex.Forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +16,32 @@ using System.Windows.Shapes;
 
 namespace ResidentialComplex
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            MainFrame.Navigate(new Hello());
+            Editor.Navigate(new Empty());
+        }
+
+        private void BtnResComp_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new ListRC());
+            Editor.Navigate(new EditData());
+        }
+
+        private void BtnHouse_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new ListHouses());
+            Editor.Navigate(new EditData());
+        }
+
+        private void BtnApartment_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new ListApartments());
+            Editor.Navigate(new EditData());
         }
     }
 }
